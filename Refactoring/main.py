2c1c154d37f590ed1202a2acf73897c90936afbb
wawan220 <mail@pohl-waldemar.info>
2025-11-19 15:14:54 +0100

    deleted free lines

--- Refactoring/main.py
+++ Refactoring/main.py
@@ -12,7 +12,6 @@ formelsammlung=[mtu.celsius_in_kelvin ,
 
 aufgabe=int(input("gebe eine Aufgabe ein z.B 1,2,3,4,5,6 usw....???"))
 
-
 if aufgabe > 0 and aufgabe <=6:
     zahl1=float(input("gebe einen Zahl ein die du umrechnen möchtest:"))
     #summe= formelsammlung[aufgabe-1](zahl1)
